b9d7e13e6de3d4689e041b94ba1997dab7e5fab6
VenBak <andre.brahin@gmail.com>
2022-12-17 20:51:07 +0100

    Saves user input into local storage and gets it back when page is refreshed

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,31 +10,47 @@
 //     console.log(data)
 // });
 
+
+
+
+// Gets all the cities from the local storage
+for (let i = 1; i <= localStorage.length; i++) {
+    var get = localStorage.getItem("citysearch"+i);
+    var getparent = document.querySelector("#searchhistory");
+    var getcreatebutton = document.createElement("button");
+    getparent.append(getcreatebutton);
+    getcreatebutton.innerHTML = get;
+    console.log(get);
+}
+
+
 // Gets the current day of the year and displays it next to city search result
 document.querySelector("#date").innerHTML = dayjs().format('(DD/MM/YYYY)');
 
-
+// Event listener for search button
 const element = document.getElementById("searchbtn");
-element.addEventListener("click", ok);
+element.addEventListener("click", searchHistory);
 
 var index = 0
 
-function ok() { 
+
+function searchHistory() {
+    // Adds 1 to an index to store the input in the local storage 
     index++;
     var input = document.getElementById("searchinput").value;
     localStorage.setItem("citysearch" + index, input);
     
+    // Adds a Button under the text input for whatever they typed in and saves it in local storage
     var parent = document.querySelector("#searchhistory");
-    var element = document.createElement("button")
+    var element = document.createElement("button");
     parent.append(element);
-    element.innerHTML = input
-    element.classList.add('button', 'btn-primary');
-
-
-
-
+    element.innerHTML = input;
+    // input.innerHTML = inputs.join('<br/>');
+    if (input != " ") {
+        inputs.push(input) 
+    }
+    // Missing: Add attribute to button
     console.log("click");
     console.log(input);
-    console.log(index)
-
+    console.log(index);
 }
